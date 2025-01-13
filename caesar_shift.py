150694e8d06488b8bf10df7a54664aca1f9127c7
MintBloom <a.stegmann09@gmail.com>
2025-01-13 21:37:05 +0000

    included decryption

--- caesar_shift.py
+++ caesar_shift.py
@@ -1,19 +1,38 @@
 
-def MessageToEncode():
-    print ("What message would you like to encode?")
-    Cypher()
-    
+def EncryptOrDecrypt():
+    print ("Decrypt or encrypt?")
+    choice = str(input())
+    if choice == "decrypt":
+        deCypher()
+    elif choice == "encrypt":
+        Cypher()
 
 def Cypher():
+    print ("what message would you like to decrypt?")
     msg = str(input())
+    print ("shift value:")
+    shift = int(input())
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     encrypt_msg = ""
-    offset = 3
     for i in msg:
         if i == " ":
             encrypt_msg += i
         else:
-            encrypt_msg += alphabet[(alphabet.find(i) + offset) % len(alphabet)]    
+            encrypt_msg += alphabet[(alphabet.find(i) + shift) % len(alphabet)]    
     print (f"encryption: {encrypt_msg}")
 
-MessageToEncode()
+def deCypher():
+    print ("what message would you like to decrypt?")
+    msg = str(input())
+    print ("shift value:")
+    shift = int(input())
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    encrypt_msg = ""
+    for i in msg:
+        if i == " ":
+            encrypt_msg += i
+        else:
+            encrypt_msg += alphabet[(alphabet.find(i) - shift) % len(alphabet)]    
+    print (f"decryption: {encrypt_msg}")
+
+EncryptOrDecrypt()
